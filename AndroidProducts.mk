4c5118a9858372cbbacc2930169daf39112941e9
Sonal Singh <sonal.singh.19993@gmail.com>
2021-04-16 01:29:23 +0530

    gram: arrow -> xtended
    Signed-off-by: Sonal Singh <sonal.singh.19993@gmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,13 +1,13 @@
 #
-# Copyright (C) 2020 The ArrowOS
+# Copyright (C) 2020 The Project Xtended
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/arrow_gram.mk
+    $(LOCAL_DIR)/xtended_gram.mk
 
 COMMON_LUNCH_CHOICES := \
-    arrow_gram-user \
-    arrow_gram-userdebug \
-    arrow_gram-eng
+    xtended_gram-user \
+    xtended_gram-userdebug \
+    xtended_gram-eng
