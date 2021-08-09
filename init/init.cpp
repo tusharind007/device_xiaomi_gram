4309b8d568db986993a50a12b65fcb6df552c56d
tushar <tusharredekar007@gmail.com>
2021-08-09 16:06:51 +0530

    gram: use source build FP

--- init/init.cpp
+++ init/init.cpp
@@ -50,17 +50,13 @@ void set_ro_build_prop(const std::string &source, const std::string &prop,
     property_override(prop_name.c_str(), value.c_str(), false);
 }
 
-void set_device_props(const std::string fingerprint, const std::string description,
-        const std::string brand, const std::string device, const std::string model) {
+void set_device_props(const std::string brand, const std::string device, const std::string model) {
     for (const auto &source : ro_props_default_source_order) {
-        set_ro_build_prop(source, "fingerprint", fingerprint);
         set_ro_build_prop(source, "brand", brand, true);
         set_ro_build_prop(source, "device", device, true);
         set_ro_build_prop(source, "model", model, true);
     }
 
-    property_override("ro.build.fingerprint", fingerprint.c_str());
-    property_override("ro.build.description", description.c_str());
 }
 
 void load_device_properties() {
@@ -68,8 +64,6 @@ void load_device_properties() {
     std::string region = GetProperty("ro.boot.hwc", "");
     if (hwname == "gram") {
         set_device_props(
-                "google/redfin/redfin:11/RQ3A.210605.005/7349499:user/release-keys",
-                "redfin-user 11 RQ3A.210605.005 7349499 release-keys",
                 "POCO", "gram", "POCO M2 Pro");
     }
 }
